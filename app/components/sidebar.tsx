1da2eb0c56fc47181a17d8c89424831abfd0b2fd
6moyan <136788608+6moyan@users.noreply.github.com>
2023-06-16 20:58:43 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -136,13 +136,16 @@ export function SideBar(props: { className?: string }) {
           onClick={() => navigate(Path.NewChat, { state: { fromHome: true } })}
           shadow
         />
-        <IconButton
-          icon={<PluginIcon />}
-          text={shouldNarrow ? undefined : Locale.Plugin.Name}
-          className={styles["sidebar-bar-button"]}
-          onClick={() => showToast(Locale.WIP)}
-          shadow
-        />
+       <a href="https://www.baidu.com" target="_blank">
+  <IconButton
+    icon={<PluginIcon />}
+    text={shouldNarrow ? undefined : Locale.Plugin.Name}
+    className={styles["sidebar-bar-button"]}
+    onClick={() => showToast(Locale.WIP)}
+    shadow
+  />
+</a>
+
       </div>
 
       <div
